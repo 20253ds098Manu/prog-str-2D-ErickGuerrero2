f331cec63c6ba590c813cd4463c0cd45acfee981
Erick Guerrero <20253ds098@utez.edu.mx>
2026-01-26 18:39:01 -0600

    Agrego actividad de codigo de la tarifa segun edad, del dia 26/01/26

---
+++ ControlIfElseErickGuerrero/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int tarifa = 0;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("¿Cual es su edad?");
+        int edad = leer.nextInt();
+        if (edad < 0 || edad > 120){
+            System.out.println("Edad invalida");
+            return;
+        }
+        System.out.println("¿Es usted un estudiante? \n Si = true No = false");
+        boolean estudiante = leer.nextBoolean();
+        if (edad < 12){
+            tarifa = 50;
+        }
+        else if (edad<=17){
+            if (estudiante){
+                tarifa = 60;
+            }
+            else{
+                tarifa = 80;
+            }
+        }
+        else if (edad>=18){
+            if (estudiante){
+                tarifa = 90;}
+            else {
+                tarifa =120;}
+
+
+        }
+        System.out.println("Usted tiene " + edad + " años");
+        if (estudiante){
+            System.out.println("Es un estudiante");
+        }
+        else{
+            System.out.println("No es un estudiante");
+        }
+        System.out.println("Su tarifa es de: " + tarifa);
+        leer.close();
+
+    }
+}
